10f1c2780d946426423c1f9da78e60abdac741de
Coding Otaku <contact@codingotaku.com>
2020-07-21 23:34:29 +0530

    added videos

--- data/nav.js
+++ data/nav.js
@@ -1,19 +1,22 @@
 export const nav = [
   {
-    name:'Coding Otaku',
-    url:'https://codingotaku.com',
-    isNewTab:true
+    name: 'Coding Otaku',
+    url: 'https://codingotaku.com',
+    isNewTab: true
   },
   {
-    name:'About',
-    url:'/pages/about',
+    name: 'About',
+    url: '/pages/about',
+  }, {
+    name: 'Videos',
+    url: '/pages/videos'
   },
   {
-    name:'Contact',
-    url:'/pages/contact',
+    name: 'Contact',
+    url: '/pages/contact',
   },
   {
-    name:'Donate',
-    url:'/pages/donate',
+    name: 'Donate',
+    url: '/pages/donate',
   }
 ];
---
+++ scripts/video-gen.js
@@ -0,0 +1,86 @@
+import { allVideos } from '/data/videos.js';
+
+export class VidGen {
+    constructor() {
+
+    }
+    generateTypes() {
+        const cardHolder = document.getElementById('card-holder');
+        // Get all available blog types and create row for each
+        allVideos.forEach((videoType) => {
+            // Add title for blog type
+            const type = this.createElement('div', ['box', 'tile', 'is-ancester']);
+            const button = this.createElement('a', ['button', 'is-primary', 'is-outlined', 'tile', 'is-2']);
+            const subtitle = this.createElement('p', ['tile']);
+            button.innerHTML = 'Watch ' + videoType.type;
+            button.href = '/pages/videos/type/?type=' + videoType.type;
+            subtitle.innerHTML = '&nbsp;' + videoType.desc;
+            type.appendChild(button);
+            type.appendChild(subtitle);
+            cardHolder.appendChild(type);
+        });
+    }
+
+    generateVideoCards(val) {
+
+        const cardHolder = document.getElementById('card-holder');
+        // Get all available blog types and create row for each
+        const videoType = allVideos.find(types => types.type === val);
+        if (!videoType) {
+            return false;
+        }
+        let columns = this.createElement('div', ['columns']);
+        // Add title for blog type
+        const type = this.createElement('div', ['title', 'has-text-dark']);
+        type.innerHTML = videoType.type;
+        cardHolder.appendChild(type);
+
+        // Create a card for each blog
+        videoType.videos.forEach((video, i) => {
+            if (i % 3 === 0) {
+                cardHolder.appendChild(columns);
+                columns = this.createElement('div', ['columns', 'is-vertical']);
+            }
+            this.generateCard(video, columns);
+        });
+
+        // Append all blogs
+        cardHolder.appendChild(columns);
+        return true;
+    }
+
+    generateCard(video, columns) {
+        const column = this.createElement('div', ['column', 'is-4']);
+        const card = this.createElement('article', ['box']);
+        const iframe = this.createElement('iframe', []);
+        const button = this.createElement('a', ['button', 'is-primary', 'is-outlined', 'is-4', 'column']);
+        button.innerHTML = 'Download';
+        button.href= 'https://lbry.tv/$/download/'+video.url;
+        iframe.src = 'https://lbry.tv/$/embed/' + video.url;
+        iframe.setAttribute('allowfullscreen', 'true')
+        iframe.width="560px"
+        iframe.height="315px"
+        card.appendChild(iframe);
+
+        const cardContent = this.createElement('div', ['card-content']);
+        const content = this.createElement('div', ['content','columns']);
+        const title = this.createElement('span', ['column']);
+
+        title.innerHTML = video.title+'&nbsp;';
+        content.appendChild(title);
+        content.appendChild(button);
+        
+
+        cardContent.appendChild(content);
+        card.appendChild(cardContent);
+        column.appendChild(card);
+        columns.appendChild(column);
+    }
+
+    createElement(element, classes) {
+        let tmpElement = document.createElement(element);
+        tmpElement.classList.add(...classes);
+        return tmpElement;
+    }
+}
+
