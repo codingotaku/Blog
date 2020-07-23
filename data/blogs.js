fe2e7de593186d60f0560191e119356d29d9ed4e
Coding Otaku <contact@codingotaku.com>
2020-07-23 09:04:52 +0530

    added article about non-free school

--- data/blogs.js
+++ data/blogs.js
@@ -4,6 +4,12 @@ export const mainBlogs = [{
       subtitle:'But is that enough?',
       image:'/images/india-chinese-app-ban/chinese-app.jpg'
     },
+    {
+      path:'/pages/non-free-school/',
+      title:'Use of Non-Free Software in School',
+      subtitle:'And what you can do against it.',
+      image:'/images/non-free-school/non-free-school.png'
+    },
   ];
 
 export const allBlogs = [];
