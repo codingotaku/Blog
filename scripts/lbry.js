091261721e07cd61089b64dded09d5d3a1d6a40f
Coding Otaku <contact@codingotaku.com>
2020-07-28 21:41:55 +0530

    keep videos in their category

--- scripts/lbry.js
+++ scripts/lbry.js
@@ -6,15 +6,6 @@ fetch('https://api.lbry.tv/api/v1/proxy', {
     body: JSON.stringify({ "method": "resolve", "params": { "urls": "@codingotaku" } })
 }).then(res => res.json()).then(res => {
     const result = res.result["@codingotaku"];
-    const banner = document.getElementById('banner');
-    banner.style.width = "100%";
-    const name = document.getElementById('name');
-    const description = document.getElementById('description');
-    const thumbnail = document.getElementById('thumbnail');
-    thumbnail.src = result.value.thumbnail.url;
-    banner.src = result.value.cover.url;
-    description.innerHTML = result.value.description;
-    name.innerHTML = result.name;
 
     fetch('https://api.lbry.tv/api/v1/proxy?m=claim_search', {
         method: 'POST',
@@ -23,36 +14,60 @@ fetch('https://api.lbry.tv/api/v1/proxy', {
         },
         body: JSON.stringify({ "method": "claim_search", "params": { "channel_ids": [result.claim_id], "page": 1, "page_size": 100 } })
     }).then(res => res.json()).then(res => {
+        const columns = createElement("div", ["columns"]);
         const items = res.result.items;
-        const listHolder = document.getElementById("list-holder");
-        let columns = createElement("div", ["columns"]);
+        let listHolder = {
+            drawing: [columns.cloneNode()],
+            privacy: [columns.cloneNode()],
+            tutorial: [columns.cloneNode()],
+            gameplay: [columns.cloneNode()],
+            misc: [columns.cloneNode()]
+        }
         items.forEach((item, i) => {
-            if (i % 4 === 0) {
-                columns = createElement("div", ["columns"]);
-                listHolder.appendChild(columns);
-            }
-            const mediaContainer = createElement("a", ["box", "column", "is-3"]);;
-            const data = createElement("p", []);
-            const img = createElement("img", []);
-            img.alt = item.name + "thumbnail";
-            img.src = item.value.thumbnail.url;
-            img.style.width = "256px";
-            data.appendChild(img);
 
-            const mediaContent = createElement("div", []);
-            const title = createElement("p", ["subtitle"]);
-            title.innerHTML = item.value.title;
-            mediaContent.appendChild(title);
-            data.appendChild(mediaContent);
-            mediaContainer.appendChild(data);
-            mediaContainer.href = item.permanent_url.replace("lbry://","https://lbry.tv/");
-            mediaContainer.target = "_blank";
-            columns.appendChild(mediaContainer); 
+            let holder = listHolder['misc'];
+            console.log(item.value.tags);
+            for (let key of Object.keys(listHolder)) {
+                if (item.value.tags.includes(key)) {
+                 holder = listHolder[key];
+                }
+            }
+            generateCard(item, holder);
         });
-        listHolder.appendChild(columns);
+        for (let key of Object.keys(listHolder)) {
+            console.log(key);
+            let list = document.getElementById(key+"-list-holder");
+            for(let column of listHolder[key] ) {
+                list.append(column);
+            }
+        }
     });
 });
 
+function generateCard(item, listHolder) {
+    if (listHolder[listHolder.length-1].children.length >= 4) {
+        let columns = createElement("div", ["columns"]);
+        listHolder.push(columns);
+    }
+    let columns = listHolder[listHolder.length-1];
+    const mediaContainer = createElement("a", ["box", "column", "is-3"]);;
+    const data = createElement("p", []);
+    const img = createElement("img", []);
+    img.alt = item.name + "thumbnail";
+    img.src = item.value.thumbnail.url;
+    img.style.width = "100%";
+    data.appendChild(img);
+
+    const mediaContent = createElement("div", []);
+    const title = createElement("p", ["subtitle"]);
+    title.innerHTML = item.value.title;
+    mediaContent.appendChild(title);
+    data.appendChild(mediaContent);
+    mediaContainer.appendChild(data);
+    mediaContainer.href = item.permanent_url.replace("lbry://", "https://lbry.tv/");
+    mediaContainer.target = "_blank";
+    columns.appendChild(mediaContainer);
+}
 function createElement(element, classes) {
     let tmpElement = document.createElement(element);
     tmpElement.classList.add(...classes);
