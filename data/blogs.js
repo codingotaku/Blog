210336f58d36fd36eaa17fcec61896c36eeb4889
Coding Otaku <contact@codingotaku.com>
2020-07-31 18:10:29 +0530

    added new blog on GNU/Linux

--- data/blogs.js
+++ data/blogs.js
@@ -1,21 +1,27 @@
 export const mainBlogs = [{
-      path:'/pages/india-chinese-app-ban/',
-      title:'India Banned Chinese Apps',
-      subtitle:'But is that enough?',
-      image:'/images/india-chinese-app-ban/chinese-app.jpg'
-    },
-    {
-      path:'/pages/non-free-school/',
-      title:'Use of Non-Free Software in School',
-      subtitle:'And what you can do against it.',
-      image:'/images/non-free-school/non-free-school.png'
-    },
-    {
-      path:'/pages/migrating-to-floss/',
-      title:'Migrating To FLOSS!',
-      subtitle:'A few alternatives to the privacy hogging softwares you use.',
-      image:'/images/migrating-to-floss/migrating-to-floss.png'
-    },
-  ];
+  path: '/pages/india-chinese-app-ban/',
+  title: 'India Banned Chinese Apps',
+  subtitle: 'But is that enough?',
+  image: '/images/india-chinese-app-ban/chinese-app.jpg'
+},
+{
+  path: '/pages/non-free-school/',
+  title: 'Use of Non-Free Software in School',
+  subtitle: 'And what you can do against it.',
+  image: '/images/non-free-school/non-free-school.png'
+},
+{
+  path: '/pages/why-do-i-say-gnu-linux',
+  title: 'Why do I say GNU/Linux?',
+  subtitle: 'My story behind loving FREE software.',
+  image: '/images/why-do-i-say-gnu-linux/gnu-linux.jpg'
+},
+{
+  path: '/pages/migrating-to-floss/',
+  title: 'Migrating To FLOSS!',
+  subtitle: 'A few alternatives to the privacy hogging softwares you use.',
+  image: '/images/migrating-to-floss/migrating-to-floss.png'
+}
+];
 
 export const allBlogs = [];
