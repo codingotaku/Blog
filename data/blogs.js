b00240b69286fd92239841fb367035dc7a1ba125
Coding Otaku <otaku@codingotaku.com>
2020-10-02 15:59:16 +0530

    UI Updates and Enhancements
    Contacts are better shown in Mobile View
    All banners are now named "banner"
    Updated "free unix" blog

--- data/blogs.js
+++ data/blogs.js
@@ -1,33 +1,38 @@
 export const mainBlogs = [
   {
+    path:'/pages/free-unix',
+    title:'Free Unix!',
+    subtitle:'New Unix implementation.',
+    image: '/images/free-unix/banner.jpeg'
+  },{
     path:'/pages/why-do-i-not-use-analytics',
     title:'Why Do I Not Use Analytics?',
     subtitle:'Not for the reason you are thinking of.',
-    image: '/images/why-do-i-not-use-analytics/no-analytics.jpg'
+    image: '/images/why-do-i-not-use-analytics/banner.jpg'
   },
   {
   path: '/pages/india-chinese-app-ban/',
   title: 'India Banned Chinese Apps',
   subtitle: 'But is that enough?',
-  image: '/images/india-chinese-app-ban/chinese-app.jpg'
+  image: '/images/india-chinese-app-ban/banner.jpg'
 },
 {
   path: '/pages/non-free-school/',
   title: 'Use of Non-Free Software in School',
   subtitle: 'And what you can do against it.',
-  image: '/images/non-free-school/non-free-school.jpg'
+  image: '/images/non-free-school/banner.jpg'
 },
 {
   path: '/pages/why-do-i-say-gnu-linux',
   title: 'Why do I say GNU/Linux?',
   subtitle: 'My story behind loving FREE software.',
-  image: '/images/why-do-i-say-gnu-linux/gnu-linux.jpg'
+  image: '/images/why-do-i-say-gnu-linux/banner.jpg'
 },
 {
   path: '/pages/migrating-to-floss/',
   title: 'Migrating To FLOSS!',
   subtitle: 'A few alternatives to the privacy hogging softwares you use.',
-  image: '/images/migrating-to-floss/migrating-to-floss.jpg'
+  image: '/images/migrating-to-floss/banner.jpg'
 }
 ];
 
