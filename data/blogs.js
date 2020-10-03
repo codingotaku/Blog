9f9388d4e3f6e93ec594b12cc323d2267d241a48
Coding Otaku <otaku@codingotaku.com>
2020-10-03 14:58:43 +0530

    New Article on Open Brain Internet Thingy

--- data/blogs.js
+++ data/blogs.js
@@ -1,44 +1,50 @@
 export const mainBlogs = [
   {
-    path:'pages/cobra-effect-on-hactoberfest',
-    title:'Cobra Effect on Hactoberfest',
-    subtitle:'What went wrong?',
-    image:'/images/cobra-effect-on-hactoberfest/banner.jpg'
+    path:"pages/open-brain-internet-thingy",
+    title:"Open Brain Internet Thingy",
+    subtitle:"Yeah, I couldn't find a name for it.",
+    image:"/images/open-brain-internet-thingy/banner.webp"
   },
   {
-    path:'/pages/free-unix',
-    title:'Free Unix!',
-    subtitle:'New Unix implementation.',
-    image: '/images/free-unix/banner.jpeg'
+    path:"pages/cobra-effect-on-hactoberfest",
+    title:"Cobra Effect on Hactoberfest",
+    subtitle:"What went wrong?",
+    image:"/images/cobra-effect-on-hactoberfest/banner.jpg"
+  },
+  {
+    path:"/pages/free-unix",
+    title:"Free Unix!",
+    subtitle:"New Unix implementation.",
+    image: "/images/free-unix/banner.jpeg"
   },{
-    path:'/pages/why-do-i-not-use-analytics',
-    title:'Why Do I Not Use Analytics?',
-    subtitle:'Not for the reason you are thinking of.',
-    image: '/images/why-do-i-not-use-analytics/banner.jpg'
+    path:"/pages/why-do-i-not-use-analytics",
+    title:"Why Do I Not Use Analytics?",
+    subtitle:"Not for the reason you are thinking of.",
+    image: "/images/why-do-i-not-use-analytics/banner.jpg"
   },
   {
-  path: '/pages/india-chinese-app-ban/',
-  title: 'India Banned Chinese Apps',
-  subtitle: 'But is that enough?',
-  image: '/images/india-chinese-app-ban/banner.jpg'
+  path: "/pages/india-chinese-app-ban/",
+  title: "India Banned Chinese Apps",
+  subtitle: "But is that enough?",
+  image: "/images/india-chinese-app-ban/banner.jpg"
 },
 {
-  path: '/pages/non-free-school/',
-  title: 'Use of Non-Free Software in School',
-  subtitle: 'And what you can do against it.',
-  image: '/images/non-free-school/banner.jpg'
+  path: "/pages/non-free-school/",
+  title: "Use of Non-Free Software in School",
+  subtitle: "And what you can do against it.",
+  image: "/images/non-free-school/banner.jpg"
 },
 {
-  path: '/pages/why-do-i-say-gnu-linux',
-  title: 'Why do I say GNU/Linux?',
-  subtitle: 'My story behind loving FREE software.',
-  image: '/images/why-do-i-say-gnu-linux/banner.jpg'
+  path: "/pages/why-do-i-say-gnu-linux",
+  title: "Why do I say GNU/Linux?",
+  subtitle: "My story behind loving FREE software.",
+  image: "/images/why-do-i-say-gnu-linux/banner.jpg"
 },
 {
-  path: '/pages/migrating-to-floss/',
-  title: 'Migrating To FLOSS!',
-  subtitle: 'A few alternatives to the privacy hogging softwares you use.',
-  image: '/images/migrating-to-floss/banner.jpg'
+  path: "/pages/migrating-to-floss/",
+  title: "Migrating To FLOSS!",
+  subtitle: "A few alternatives to the privacy hogging softwares you use.",
+  image: "/images/migrating-to-floss/banner.jpg"
 }
 ];
 
